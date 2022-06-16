8b235cf9697627fa9a00054f9a80f2e3dda56341
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2022-06-16 14:57:33 +0200

    add javadoc comments in StringQueue and IQueue

--- src/main/java/at/fhj/msd/IQueue.java
+++ src/main/java/at/fhj/msd/IQueue.java
@@ -1,29 +1,38 @@
 package at.fhj.msd;
 
-// fastly written (not really nice) comments but good enough to understand
+/**
+ * @author Isabella Kainer, Kathrin Hubmann
+ *
+ */
 public interface IQueue {
-  /* 
+  /**
   add object to queue, return true if works, otherwise false
+   @param obj
+   @return boolean
   */
   public abstract boolean offer(String obj);
 
-  /* 
+  /**
   returns and deletes first element; null if no element in there
+   @return String
    */
   public abstract String poll();
 
-  /* 
+  /**
   like poll but if no elment exists NoSuchElementException is throwin instead of null return value
+   @return String
    */
   public abstract String remove();
 
-  /* 
+  /**
   gives first element but does not delete, null if nothing there
+   @return String
    */
   public abstract String peek();
 
-  /*
+  /**
   like peek but NoSuchElementException instead of null
+   @return String
    */
   public abstract String element();
 }
--- src/main/java/at/fhj/msd/StringQueue.java
+++ src/main/java/at/fhj/msd/StringQueue.java
@@ -8,15 +8,28 @@ import java.util.NoSuchElementException;
 // there are different Bugs, wrong implementation, typos, ...
 // write Test-Cases (read Queue Interface for understanding methods) and use Debugging possibilies of your IDE
 
+/**
+ * @author Isabella Kainer, Kathrin Hubmann
+ */
+
 public class StringQueue implements IQueue {
 
   private List<String> elements = new ArrayList<String>();
   private int maxSize = 5;
 
+  /**
+   * Constructor
+   * @param maxSize
+   */
   public StringQueue(int maxSize) {
     this.maxSize = maxSize;
   }       //maxsize -> maxSize; other maxSize -> this.maxSize
 
+  /**
+   * adds element to Queue
+   * @param obj
+   * @return boolean
+   */
   @Override
   public boolean offer(String obj) {
     if (elements.size() != maxSize)
@@ -27,17 +40,27 @@ public class StringQueue implements IQueue {
     return true;
   }
 
+  /**
+   * takes element from queue and removes it - does not throw exception
+   * @return String
+   */
   @Override
   public String poll() {
     String element = peek();
 
-    if (elements.size() == 0) {
-      return "String is empty";                                   //can't remove an element if nothing is there
+    if (elements.size() > 0) {
+      elements.remove(element);                                   //can't remove an element if nothing is there
+    } else {
+      return null;
     }
 
     return element;
   }
 
+  /**
+   * takes element from poll() - throws exception
+   * @return String
+   */
   @Override
   public String remove() {
     String element = poll();                                    //if you poll you shouldn't override it with ""
@@ -47,6 +70,10 @@ public class StringQueue implements IQueue {
     return element;
   }
 
+  /**
+   * takes element from queue - does not throw exception
+   * @return String
+   */
   @Override
   public String peek() {
     String element;
@@ -58,6 +85,10 @@ public class StringQueue implements IQueue {
     return element;
   }
 
+  /**
+   * takes element from peek() - throws exception
+   * @return String
+   */
   @Override
   public String element() {
     String element = peek();
